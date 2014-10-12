6dc13f9f3e8035f5c87c6c65095a65292e353fae
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-10-12 15:07:25 +0800

    Fast update.

--- server/static.js
+++ server/static.js
@@ -5,10 +5,11 @@ var app = express( );
 
 app.use( compression( ) );
 
-app.use( express.static( path.resolve( "crime-app/deploy" ) ) );
+app.use( express.static( path.resolve( __dirname, "deploy" ) ) );
 
 app.use( function onRequest( request, response, next ){
-	response.send( 404, "404: Welcome to the dark side." );
+	response.status( 404 )
+			.send( "404: Welcome to the dark side." );
 } );
 
 app.listen( process.env.PORT || 8080 );
