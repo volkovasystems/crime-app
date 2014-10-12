6fee38928474b69effac02a28cef4a2a3ba69383
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-10-13 07:35:00 +0800

    Bring back to deploy mode.

--- server/static.js
+++ server/static.js
@@ -5,7 +5,7 @@ var app = express( );
 
 app.use( compression( ) );
 
-app.use( express.static( "build" ) );
+app.use( express.static( "deploy" ) );
 
 app.use( function onRequest( request, response, next ){
 	response.status( 404 )
