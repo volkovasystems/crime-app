11b4c12680c5d29be0fbf75a3d186f88680b853d
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-11-22 19:23:22 +0800

    Fixed getting the raw html of the self dom.

--- client/script/icon.js
+++ client/script/icon.js
@@ -245,7 +245,7 @@ angular.module( "Icon", [ ] )
 
 				"attachSVGElement": function attachSVGElement( svgElement ){
 					this.setState( {
-						"rawSVGElement": $( "<div>" ).append( svgElement ).html( ),
+						"rawSVGElement": $( "<div>" ).append( svgElement.clone( ) ).html( ),
 						"svgSourceState": "svg-ready"
 					} );
 				},
